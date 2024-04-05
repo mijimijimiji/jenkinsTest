18dd185d8c48cef18004663a9ccebb69e08703cb
user <user@BOOK-UP5F0RS3G7>
2024-04-05 12:12:31 +0900

    3

--- boot13_MVC3_jsp_mybatis/src/main/java/com/example/Boot13Mvc1Jsp1Application.java
+++ boot13_MVC3_jsp_mybatis/src/main/java/com/example/Boot13Mvc1Jsp1Application.java
@@ -2,12 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Boot13Mvc1Jsp1Application {
-
+public class Boot13Mvc1Jsp1Application extends SpringBootServletInitializer{
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(Boot13Mvc1Jsp1Application.class, args);
 	}
 
+	
+/////////////////////추가부분
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//TODO Auto-generated method stub
+return builder.sources(Boot13Mvc1Jsp1Application.class);////////////
+}
+//////////////////////////////////
 }
